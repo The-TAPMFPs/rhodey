6c3475abf94e09369a18e0a6ab1a380ce767a474
Rec1dite <rec1dite@gmail.com>
2022-10-08 11:55:15 +0200

    Added FTXUI sample project in /Code

---
+++ Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/animation.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/animation.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/button.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/button.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/catch_event.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/catch_event.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/checkbox.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/checkbox.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/collapsible.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/collapsible.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/component.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/component.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/component_options.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/component_options.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/container.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/container.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/dropdown.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/dropdown.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/event.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/event.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/input.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/input.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/maybe.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/maybe.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/menu.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/menu.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/radiobox.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/radiobox.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/renderer.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/renderer.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/resizable_split.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/resizable_split.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/screen_interactive.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/screen_interactive.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/slider.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/slider.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/terminal_input_parser.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/terminal_input_parser.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/component/util.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/component.dir/src/ftxui/component/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "_deps/ftxui-src/include"
+  "_deps/ftxui-src/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/DependInfo.cmake"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/screen.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/automerge.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/automerge.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/blink.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/blink.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/bold.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/bold.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/border.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/border.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/box_helper.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/box_helper.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/canvas.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/canvas.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/clear_under.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/clear_under.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/color.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/color.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/composite_decorator.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/composite_decorator.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/dbox.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/dbox.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/dim.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/dim.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/flex.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flex.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/flexbox.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/flexbox_helper.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_helper.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/focus.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/focus.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/frame.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/frame.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/gauge.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/gauge.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/graph.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/graph.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/gridbox.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/gridbox.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/hbox.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/hbox.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/inverted.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/inverted.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/node.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/node.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/node_decorator.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/node_decorator.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/paragraph.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/paragraph.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/reflect.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/reflect.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/scroll_indicator.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/scroll_indicator.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/separator.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/separator.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/size.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/size.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/spinner.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/spinner.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/table.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/table.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/text.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/text.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/underlined.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/underlined.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/util.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/util.cpp.o"
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-src/src/ftxui/dom/vbox.cpp" "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/vbox.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "_deps/ftxui-src/include"
+  "_deps/ftxui-src/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/Users/Rec1dite/Desktop/214_pa5/Code/ftxui-starter/build/_deps/ftxui-build/CMakeFiles/screen.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
