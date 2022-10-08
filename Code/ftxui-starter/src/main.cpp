942358c15405e3ef3209ba51adc0f2a172b925c8
Rec1dite <rec1dite@gmail.com>
2022-10-08 15:45:13 +0200

    Began roughing out the UI layout

--- Code/ftxui-starter/src/main.cpp
+++ Code/ftxui-starter/src/main.cpp
@@ -1,39 +1,122 @@
-#include <iostream>
+#include <functional>
+#include <memory>      // for shared_ptr, allocator, __shared_ptr_access
+#include <string>
+#include <vector>
+ 
+#include "ftxui/component/captured_mouse.hpp"       // for ftxui
+#include "ftxui/component/component.hpp"            // for Slider, Checkbox, Vertical, Renderer, Button, Input, Menu, Radiobox, Toggle
+#include "ftxui/component/component_base.hpp"       // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp"   // for Component, ScreenInteractive
+#include "ftxui/dom/elements.hpp"                   // for separator, operator|, Element, size, xflex, text, WIDTH, hbox, vbox, EQUAL, border, GREATER_THAN
+#include "ftxui/dom/canvas.hpp"                     // for Canvas
+#include "ftxui/dom/node.hpp"                       // for Render
+#include "ftxui/screen/color.hpp"                   // for Color, Color::Red, Color::Blue, Color::Green, ftxui
+ 
+using namespace ftxui;
+ 
+// Display a component nicely with a title on the left.
+//TODO: Remove since we probably won't need this
+Component Wrap(std::string name, Component component) {
+  return Renderer(component, [name, component] {
+    return hbox({
+               text(name) | size(WIDTH, EQUAL, 8),
+               separator(),
+               component->Render() | xflex,
+           }) |
+           xflex;
+  });
+}
 
-#include "ftxui/dom/elements.hpp"
-#include "ftxui/screen/screen.hpp"
-#include "ftxui/screen/string.hpp"
 
-int main(void) {
-  using namespace ftxui;
 
-  auto summary = [&] {
-    auto content = vbox({
-        hbox({text(L"- done:   "), text(L"3") | bold}) | color(Color::Green),
-        hbox({text(L"- active: "), text(L"2") | bold}) | color(Color::RedLight),
-        hbox({text(L"- queue:  "), text(L"9") | bold}) | color(Color::Red),
-    });
-    return window(text(L" Summary "), content);
+int main(int argc, const char* argv[]) {
+  auto screen = ScreenInteractive::Fullscreen();
+
+  //===== CREATE SCREEN CONTAINERS =====//
+
+#pragma region MAIN_PANEL
+  
+  //CREATE MENU
+  const std::vector<std::string> menu_entries = {
+      "Menu 1",
+      "Menu 2",
+      "Menu 3",
+      "Menu 4",
   };
+  int menu_selected = 0;
+  auto menu = Menu(&menu_entries, &menu_selected);
+  menu = Wrap("Menu", menu);
 
-  auto document =  //
-      vbox({
-          hbox({
-              summary(),
-              summary(),
-              summary() | flex,
-          }),
-          summary(),
-          summary(),
+  //CREATE BUTTON
+  std::string button_label = "Quit";
+  std::function<void()> on_button_clicked_;
+  auto button = Button(&button_label, screen.ExitLoopClosure());
+  // button = Wrap("Button", button);
+
+  //PANEL LAYOUT
+  auto layout = Container::Vertical({
+      button,
+      menu
+  });
+
+  //=====MIDDLE PANEL=====//
+  auto middle = Renderer(layout, [&] {
+      return vbox({
+        menu->Render(),
+        separator(),
+        (text("Hello world") | center),
+        separator(),
+        button->Render()
       });
+  });
+#pragma endregion
 
-  // Limit the size of the document to 80 char.
-  document = document | size(WIDTH, LESS_THAN, 80);
 
-  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
-  Render(screen, document);
+#pragma region TOP_PANEL
+  //=====TOP PANEL=====//
+  auto top = Renderer([] { return text("TOP PANEL") | center; });
+#pragma endregion
 
-  std::cout << screen.ToString() << '\0' << std::endl;
 
-  return EXIT_SUCCESS;
+#pragma region RIGHT_PANEL
+  //GAME MAP
+  int mapW = 50, mapH = 50;
+  auto mapCanvas = Canvas(mapW, mapH);
+
+  mapCanvas.DrawPointLine(0, 0, mapW-1, mapH-1, Color::Red);
+  mapCanvas.DrawPointLine(0, mapH-1, mapW-1, 0, Color::Red);
+
+  mapCanvas.DrawText(mapW/4, mapH/2, "MAP GOES HERE :)", [](Pixel& p) {
+    p.foreground_color = Color::Aquamarine1;
+  });
+
+  //=====RIGHT PANEL=====//
+  auto right = Renderer([&] {
+    return canvas(&mapCanvas) | hcenter;
+  });
+#pragma endregion
+
+
+#pragma region BOTTOM_PANEL
+  //=====BOTTOM PANEL=====//
+  auto bottom = Renderer([] { return text("BOTTOM PANEL") | center; });
+#pragma endregion
+ 
+
+#pragma region MAIN_PANEL
+  //Default starting sizes for each panel
+  int right_size = 40;
+  int top_size = 1;
+  int bottom_size = 2;
+ 
+  auto container = middle; // The main container holding all the window panels
+  container = ResizableSplitTop(top, container, &top_size);
+  container = ResizableSplitRight(right, container, &right_size);
+  container = ResizableSplitBottom(bottom, container, &bottom_size);
+
+  //=====MAIN PANEL=====//
+  auto renderer = Renderer(container, [&] { return container->Render() | border; }); //The global container renderer
+#pragma endregion
+
+  screen.Loop(renderer);
 }
