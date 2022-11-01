9d736084ab283ff7128a916189cba5d1b98896c5
cadendobson <cadenjared4@gmail.com>
2022-11-01 20:53:24 +0200

    fixed compilation issue

---
+++ Code/ftxui-starter/src/Entities/WeaponTemplateMethod/Weapons.cpp
@@ -0,0 +1,25 @@
+#include "Weapons.h"
+
+Bazooka::Bazooka() : Weapon(0, "Bazooka") {
+  this->damage = 90 + rand() % 100;  // DO RANDOM DAMAGE GENERATION
+}
+
+Sniper::Sniper() : Weapon(0, "Sniper") {
+  this->damage = 85 + rand() % 100;
+}
+
+Sniper50::Sniper50() : Weapon(0, "Sniper50") {
+  this->damage = 90 + rand() % 100;
+}
+
+Pistol::Pistol() : Weapon(0, "Pistol") {
+  this->damage = 5 + rand() % 15;
+}
+
+AR::AR() : Weapon(0, "AR") {
+  this->damage = 15 + rand() % 30;
+}
+
+SMG::SMG() : Weapon(0, "SMG") {
+  this->damage = 10 + rand() % 25;
+}
---
+++ Code/ftxui-starter/src/Entities/WeaponTemplateMethod/Weapons.h
@@ -0,0 +1,25 @@
+#include "Weapon.h"
+
+class Bazooka : public Weapon {
+  Bazooka();
+};
+
+class Sniper : public Weapon{
+    Sniper();
+};
+
+class Sniper50 : public Weapon{
+    Sniper50();
+};
+
+class Pistol : public Weapon{
+    Pistol();
+};
+
+class AR : public Weapon{
+    AR();
+};
+
+class SMG : public Weapon{
+    SMG();
+};
