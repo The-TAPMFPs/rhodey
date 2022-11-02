900c4b4edb94537d433a166d5b4b3d7baf53f5d3
Christiaan Lombard <christiaanlombard12@gmail.com>
2022-11-02 22:12:19 +0200

    Added the decision process for deciding the next strategy for a country during its turn

--- Code/ftxui-starter/src/Country/Country.cpp
+++ Code/ftxui-starter/src/Country/Country.cpp
@@ -18,6 +18,9 @@ Country::Country(std::string name) : name(name) {
     aggressiveness = 0;
     goalRating = 0;
     numSpies = 0;
+    numTroops = 0;
+    numVehicles = 0;
+    numEnemyRegions = 0;
     generatePersonalityMatrix();
 }
 
@@ -29,14 +32,124 @@ std::string Country::getName()
 void Country::generatePersonalityMatrix()
 {
     Eigen::MatrixXd pm(6, 18);
+    double* offensiveVals = generateRandomNums(4);
+    double* defensiveVals = generateRandomNums(4);
+    double* developVals = generateRandomNums(3);
+    double* prepVals = generateRandomNums(3);
+    double* intelVals = generateRandomNums(1);
+    double* diploVals = generateRandomNums(4);
 
     pm <<
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+        offensiveVals[0], 0, offensiveVals[1], 0, 0, 0, 0, 0, 0, offensiveVals[2], 0, 0, offensiveVals[3], 0, 0, 0, 0, 0, //offensive
+        defensiveVals[0], 0, defensiveVals[1], 0, 0, 0, 0, 0, 0, defensiveVals[2], 0, 0, defensiveVals[3], 0, 0, 0, 0, 0, //defensive
+        0, 0, 0, 0, 0, developVals[0], 0, developVals[1], 0, 0, 0, 0, 0, 0, 0, 0, 0, developVals[2], //development
+        0, prepVals[0], 0, prepVals[1], prepVals[2], 0, 0, prepVals[3], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //preparation
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, intelVals[0], 0, 0, 0, 0, 0, 0, //intel
+        0, 0, 0, 0, 0, diploVals[0], 0, diploVals[1], 0, 0, 0, 0, 0, diploVals[2], diploVals[3], 0, 0, 0; //diplomacy
 
     this->personalityMatrix = pm;
+}
+
+Eigen::MatrixXd Country::generateValueMatrix()
+{
+    Eigen::MatrixXd valMatrix(18, 1);
+    valMatrix <<
+        numTroops/(0.3*population),
+        1-(numTroops/(0.3*population)),
+        numVehicles/(numTroops/10),
+        1-(numVehicles/(numTroops/10)),
+        economy,
+        1-economy,
+        resources,
+        1-resources,
+        morale,
+        1-morale,
+        numSpies/(numEnemyRegions/5),
+        1-(numSpies/(numEnemyRegions/5)),
+        aggressiveness,
+        1-aggressiveness,
+        goalRating,
+        1-goalRating,
+        research,
+        1-research;
+
+        return valMatrix;
+}
+
+
+double* Country::generateRandomNums(int num)
+{
+    srand((unsigned)time(NULL));
+
+    double* vals = new double[num];
+    double sum=0;
+	for(int i = 0; i<num; i++)
+	{
+		vals[i] = (double) rand()/RAND_MAX;//array initialisation
+        sum+=vals[i];
+	}
+
+    for(int i = 0; i<num; i++)
+	{
+		vals[i] /= sum;
+	}
+
+	return vals; 
+
+}
+
+void Country::setStrategy(BattleStrategy* strategy)
+{
+    this->strategy = strategy;
+}
+
+int Country::nextStrategy()
+{
+    Eigen::MatrixXd valMatrix = generateValueMatrix();
+    Eigen::MatrixXd pm = this->personalityMatrix;
+    Eigen::MatrixXd result = pm*valMatrix;
+    int maxIndex = 0;
+    double maxVal = 0;
+    for(int i = 0; i<result.rows(); i++)
+    {
+        if(result(i,0) > maxVal)
+        {
+            maxVal = result(i,0);
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
+
+
+void Country::takeTurn()
+{
+    /* int nextStrat = nextStrategy();
+
+    switch(nextStrat)
+    {
+        case 0:
+            this->strategy = new Offensive(red);
+            break;
+         case 1:
+            this->strategy = new Defensive();
+            break;
+        case 2:
+            this->strategy = new ResearchAndDevelopment();
+            break;
+        case 3:
+            this->strategy = new PreparationStrategy();
+            break;
+        case 4:
+            this->strategy = new IntelligenceStrategy();
+            break;
+        case 5:
+            this->strategy = new DiplomacyStrategy();
+            break; 
+        default:
+            this->strategy = new Offensive(red);
+            break;
+    } */
+    
 }
