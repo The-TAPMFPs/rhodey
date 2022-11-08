29524bd65a8188c32122d2951bc56662ea058ee8
Rec1dite <rec1dite@gmail.com>
2022-11-08 07:12:06 +0200

    Fixed logger segfault

--- Code/ftxui-starter/src/logger.cpp
+++ Code/ftxui-starter/src/logger.cpp
@@ -1,15 +1,17 @@
 #include "logger.h"
 
+std::vector<std::string> Logger::messages = {"ACTIONS WILL APPEAR HERE"};
 std::string Logger::message = "ACTIONS WILL APPEAR HERE";
 
 void Logger::log(std::string message)
 {
-    Logger::message += message;
+    // Logger::messages.push_back(message);
+    Logger::message = message;
 }
 
 std::string Logger::getMsg()
 {
+    // std::string toReturn = Logger::messages.back();
     std::string toReturn = Logger::message;
-    Logger::message = "";
     return toReturn;
 }
--- Code/ftxui-starter/src/logger.h
+++ Code/ftxui-starter/src/logger.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <sstream>
 #include <string>
+#include <vector>
 
 class Logger {
  public:
   static std::string message;
+  static std::vector<std::string> messages;
 
  public:
   static void log(std::string message);
